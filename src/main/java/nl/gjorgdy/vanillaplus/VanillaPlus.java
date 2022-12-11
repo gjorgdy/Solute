b0664d4aec233c926dc6ebc073958276884caeee
Jordy <jordyreins@gmail.com>
2022-12-12 00:26:00 +0100

    Succesfully added an implementation of BlocksMixin

---
+++ src/main/java/nl/gjorgdy/vanillaplus/VanillaPlus.java
@@ -0,0 +1,27 @@
+package nl.gjorgdy.vanillaplus;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+import nl.gjorgdy.vanillaplus.blocks.PurpurBlock;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class VanillaPlus implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("VanillaPlus");
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world! Your leader speaking, please do what I say");
+	}
+}
