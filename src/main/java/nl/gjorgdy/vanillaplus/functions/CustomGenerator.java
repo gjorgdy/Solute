0f9259e2bd5a66afedacefa63d9e46ac53701ea5
Jordy <jordyreins@gmail.com>
2022-12-19 00:50:22 +0100

    Finished the implementation of the config into the functions, using registry reading

--- src/main/java/nl/gjorgdy/vanillaplus/functions/CustomGenerator.java
+++ src/main/java/nl/gjorgdy/vanillaplus/functions/CustomGenerator.java
@@ -3,13 +3,16 @@ package nl.gjorgdy.vanillaplus.functions;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.registry.Registries;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import nl.gjorgdy.vanillaplus.VanillaPlus;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
+
+import static nl.gjorgdy.vanillaplus.functions.BlockFunctions.getBlockFromID;
 
 public class CustomGenerator {
 
@@ -17,18 +20,23 @@ public class CustomGenerator {
 
     public static BlockState replace(World world, BlockPos pos, BlockState block) {
         Block under = world.getBlockState(pos.add(0, -1, 0)).getBlock();
-        if (world.getFluidState(pos).isStill()) {
+        if (block.getBlock() == Blocks.OBSIDIAN) {
             return generateObsidian(world, pos);
-        } else if (contains(under)) {
-            return under.getDefaultState();
+        } else if (block.getBlock() == Blocks.COBBLESTONE) {
+            return replacementMap(under).getDefaultState();
         } else {
-            return Blocks.COBBLESTONE.getDefaultState();
+            return block;
         }
     }
 
-    private static boolean contains(Block block) {
-        String id = block.toString().split("\\{|\\}")[1];
-        return VanillaPlus.CONFIG.generatorBlocks().containsKey(id);
+    private static Block replacementMap(Block block) {
+        Map<String, String> config = VanillaPlus.CONFIG.generatorBlocks();
+        for (String id : config.keySet()) {
+            if (getBlockFromID(id) == block) {
+                return getBlockFromID( config.get(id) );
+            }
+        }
+        return Blocks.COBBLESTONE;
     }
 
     private static BlockState generateObsidian(World world, BlockPos pos) {
