face9f98ad6a1ca85d6062a5e825e1253f0f5d87
Jordy <jordyreins@gmail.com>
2025-01-16 18:00:07 +0100

    1.21.4 FIX: Fixed explosion drop logic

--- src/main/java/nl/gjorgdy/solute/mixins/tnt/ExplosionMixin.java
+++ src/main/java/nl/gjorgdy/solute/mixins/tnt/ExplosionMixin.java
@@ -1,13 +1,19 @@
 package nl.gjorgdy.solute.mixins.tnt;
 
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
 import net.minecraft.world.explosion.ExplosionImpl;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArgs;
-import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+import java.util.Collection;
+import java.util.List;
 import java.util.Random;
 
 @Mixin(ExplosionImpl.class)
@@ -16,31 +22,42 @@ public class ExplosionMixin {
     @Unique
     private static final Random random = new Random();
 
-    @ModifyArgs(method = "destroyBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;dropStack(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/item/ItemStack;)V"))
-    private void affectWorld(Args args) {
-        args.set(2, getDrop(args.get(2)));
+    @SuppressWarnings("InjectedReferences")
+    @Redirect(method = "destroyBlocks", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/Block;dropStack(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/item/ItemStack;)V"))
+    private void affectWorld(World world, BlockPos pos, ItemStack stack) {
+        Collection<ItemStack> drops = getDrops(stack);
+        if (drops == null) Block.dropStack(world, pos, stack);
+        else drops.forEach(_stack -> Block.dropStack(world, pos, _stack));
     }
 
     @Unique
-    private static ItemStack getDrop(ItemStack drop) {
+    private static Collection<ItemStack> getDrops(ItemStack drop) {
+        int wholeDropCount = 0;
+        Item shatteredDrop = null;
+        int shatteredDropCount = 0;
 
-        if (drop.isOf(Items.COBBLESTONE)) {
-            int count = random.nextInt(7) - 2;
-            if (count > 0) {
-                drop = new ItemStack(Items.GRAVEL, count * drop.getCount());
-            }
-        } else if (drop.isOf(Items.SANDSTONE)) {
-            int count = random.nextInt(11) - 5;
-            if (count > 0) {
-                drop = new ItemStack(Items.SAND, drop.getCount() * count);
-            }
-        } else if (drop.isOf(Items.RED_SANDSTONE)) {
-            int count = random.nextInt(11) - 5;
-            if (count > 0) {
-                drop = new ItemStack(Items.RED_SAND, drop.getCount() * count);
+        for (int i = 0; i < drop.getCount(); i++) {
+            if (drop.isOf(Items.COBBLESTONE)) {
+                int count = random.nextInt(7) - 2;
+                if (count > 0) shatteredDropCount += count;
+                else wholeDropCount++;
+                shatteredDrop = Items.GRAVEL;
+            } else if (drop.isOf(Items.SANDSTONE)) {
+                int count = random.nextInt(10) - 5;
+                if (count > 0) shatteredDropCount += count;
+                else wholeDropCount++;
+                shatteredDrop = Items.SAND;
+            } else if (drop.isOf(Items.RED_SANDSTONE)) {
+                int count = random.nextInt(10) - 5;
+                if (count > 0) shatteredDropCount += count;
+                else wholeDropCount++;
+                shatteredDrop = Items.RED_SAND;
+            } else {
+                return null;
             }
         }
-        return drop;
+        if (shatteredDrop == null) return null;
+        return List.of(drop.copyWithCount(wholeDropCount), new ItemStack(shatteredDrop, shatteredDropCount));
     }
 
 }
