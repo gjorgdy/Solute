903f9abc56bcd946ff1ed8d48405a9bf5566908b
Jordy <jordyreins@gmail.com>
2025-01-18 01:37:26 +0100

    1.21.4 IMP: Improved the logic for growing grass with bone meal.

--- src/main/java/nl/gjorgdy/solute/mixins/grass/BoneMealItemMixin.java
+++ src/main/java/nl/gjorgdy/solute/mixins/grass/BoneMealItemMixin.java
@@ -1,77 +1,81 @@
 package nl.gjorgdy.solute.mixins.grass;
 
 import net.minecraft.block.Blocks;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Vec3i;
 import net.minecraft.world.World;
+import nl.gjorgdy.solute.utils.BlockPosUtils;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
 
 @Mixin(BoneMealItem.class)
 public class BoneMealItemMixin {
 
     @Unique
-    private static final List<Vec3i> offsets = List.of(
-            //top
-            new Vec3i(0, 1, -1),
-            new Vec3i(-1, 1, 0),
-            new Vec3i(0, 1, 0),
-            new Vec3i(1, 1, 0),
-            new Vec3i(0, 1, 1),
-            //middle
-            new Vec3i(-1, 0, -1),
-            new Vec3i(0, 0, -1),
-            new Vec3i(1, 0, -1),
-            new Vec3i(-1, 0, 0),
-            new Vec3i(0, 0, 0),
-            new Vec3i(1, 0, 0),
-            new Vec3i(-1, 0, 1),
-            new Vec3i(0, 0, 1),
-            new Vec3i(1, 0, 1),
-            //bottom
-            new Vec3i(0, -1, -1),
-            new Vec3i(-1, -1, 0),
-            new Vec3i(0, -1, 0),
-            new Vec3i(1, -1, 0),
-            new Vec3i(0, -1, 1)
-    );
-
-    @Unique
-    private static void spreadGrass(World world, BlockPos pos, int radius) {
+    private static void growGrass(World world, BlockPos pos) {
         world.setBlockState(pos, Blocks.GRASS_BLOCK.getDefaultState());
-        if (radius == 0) return;
-        offsets.forEach(os -> {
-            BlockPos _pos = pos.add(os);
-            int r = Math.abs(os.getX()) + Math.abs(os.getY()) + Math.abs(os.getZ()) + 1;
-            if (world.getBlockState(_pos).isOf(Blocks.DIRT) && world.getRandom().nextInt(r) == 0) {
-                spreadGrass(world, _pos, radius - 1);
-            }
-        });
     }
 
-    @Inject(method = "useOnBlock", at = @At(value = "RETURN"))
+    @Unique
+    private static void spreadGrass(World world, BlockPos pos, int depth) {
+        if (world.getBlockState(pos).isOf(Blocks.DIRT) || world.getBlockState(pos).isOf(Blocks.GRASS_BLOCK)) {
+            int grassNeighbours = countGrassNeighbors(world, pos);
+            if (grassNeighbours > 0 && world.getRandom().nextInt(Math.max(4 - grassNeighbours, 1)) == 0) {
+                growGrass(world, pos);
+                if (depth > 0) BlockPosUtils.forNeighbours(
+                    pos,
+                    _pos -> spreadGrass(world, _pos, depth - 1)
+                );
+            }
+        }
+    }
+
+    @Unique
+    private static int countGrassNeighbors(World world, BlockPos pos) {
+        AtomicInteger grassBlockCount = new AtomicInteger();
+        BlockPosUtils.forDirectNeighbours(pos, _pos -> {
+            if (world.getBlockState(_pos).isOf(Blocks.GRASS_BLOCK))
+                grassBlockCount.getAndIncrement();
+        });
+        return grassBlockCount.get();
+    }
+
+    @Unique
+    private static void success(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+        PlayerEntity player = context.getPlayer();
+        if (player != null) {
+            player.swingHand(context.getHand(), true);
+            context.getStack().decrementUnlessCreative(1, player);
+            cir.setReturnValue(ActionResult.SUCCESS);
+        }
+    }
+
+    @Inject(method = "useOnBlock", at = @At(value = "RETURN"), cancellable = true)
     private void useOnGrass(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
         World w = context.getWorld();
         BlockPos bp = context.getBlockPos();
-        if (w.getBlockState(bp).isOf(Blocks.GRASS_BLOCK))
-            spreadGrass(w, bp, 3);
+        if (w.getBlockState(bp).isOf(Blocks.GRASS_BLOCK)) {
+            spreadGrass(w, bp, 2);
+            if (cir.getReturnValue() != ActionResult.SUCCESS) success(context, cir);
+        }
     }
 
     @Inject(method = "useOnBlock", at = @At(value = "RETURN", ordinal = 2), cancellable = true)
     private void useOnDirt(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
-        World w = context.getWorld();
-        BlockPos bp = context.getBlockPos();
-        if (w.getBlockState(bp).isOf(Blocks.DIRT)) {
-            spreadGrass(w, bp, 1);
-            cir.setReturnValue(ActionResult.SUCCESS);
+        World world = context.getWorld();
+        BlockPos pos = context.getBlockPos();
+        if (world.getBlockState(pos).isOf(Blocks.DIRT)) {
+            spreadGrass(world, pos, 1);
+            success(context, cir);
         }
     }
 }
