5a3b0e3cc8b5e45c4faf287a60f604228056f0dd
Jordy <jordyreins@gmail.com>
2025-07-05 19:49:21 +0200

    1.21.5 DEL: moved enchantments, crafting and shulker modules to extras mod

--- src/main/java/nl/gjorgdy/solute/Solute.java
+++ src/main/java/nl/gjorgdy/solute/Solute.java
@@ -2,15 +2,11 @@ package nl.gjorgdy.solute;
 
 import me.fzzyhmstrs.fzzy_config.api.ConfigApiJava;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
 import net.fabricmc.fabric.api.event.player.PlayerBlockBreakEvents;
 import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.minecraft.enchantment.Enchantment;
-import net.minecraft.registry.entry.RegistryEntry;
 import nl.gjorgdy.solute.config.SoluteConfig;
 import nl.gjorgdy.solute.listeners.PlayerBlockBreakListener;
 import nl.gjorgdy.solute.listeners.UseBlockCallbackListener;
-import nl.gjorgdy.solute.utils.EnchantmentUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -27,31 +23,9 @@ public class Solute implements ModInitializer {
 		LOGGER.info("Igniting furnace to add solute to the base");
 
 		UseBlockCallback.EVENT.register(new UseBlockCallbackListener());
-		PlayerBlockBreakEvents.BEFORE.register(new PlayerBlockBreakListener());
 
-		ServerLifecycleEvents.SERVER_STARTED.register(server -> {
-			if (CONFIG.enchantmentsModule.enabled && CONFIG.enchantmentsModule.excavation) {
-				// Excavation
-				var excavation = EnchantmentUtils.getEnchantmentFromString(server, "solute:excavation");
-				excavation.ifPresent(enchantmentReference -> ENCHANTMENTS.EXCAVATION = enchantmentReference);
-			}
-			if (CONFIG.enchantmentsModule.enabled && CONFIG.enchantmentsModule.drilling) {
-				// Drilling
-				var drilling = EnchantmentUtils.getEnchantmentFromString(server, "solute:drilling");
-				drilling.ifPresent(enchantmentReference -> ENCHANTMENTS.DRILLING = enchantmentReference);
-			}
-			if (CONFIG.enchantmentsModule.enabled && CONFIG.enchantmentsModule.crushing) {
-				// Crushing
-				var crushing = EnchantmentUtils.getEnchantmentFromString(server, "solute:crushing");
-				crushing.ifPresent(enchantmentReference -> ENCHANTMENTS.CRUSHING = enchantmentReference);
-			}
-		});
+		PlayerBlockBreakEvents.AFTER.register(new PlayerBlockBreakListener());
 
 	}
 
-	public static class ENCHANTMENTS {
-		public static RegistryEntry.Reference<Enchantment> EXCAVATION;
-		public static RegistryEntry.Reference<Enchantment> DRILLING;
-		public static RegistryEntry.Reference<Enchantment> CRUSHING;
-	}
 }
