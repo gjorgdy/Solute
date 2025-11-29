10ad86d856dd09d18b847c5c48fa648713e0bd5a
Gjorgdy <jordy.reins@proton.me>
2025-11-29 17:20:42 +0100

    1.21.10 ADD: un-till farmland using a hoe

--- src/main/java/nl/gjorgdy/solute/mixins/farmland/HoeItemMixin.java
+++ src/main/java/nl/gjorgdy/solute/mixins/farmland/HoeItemMixin.java
@@ -2,11 +2,15 @@ package nl.gjorgdy.solute.mixins.farmland;
 
 import me.fallenbreath.conditionalmixin.api.annotation.Condition;
 import me.fallenbreath.conditionalmixin.api.annotation.Restriction;
+import net.minecraft.block.Blocks;
 import net.minecraft.block.CropBlock;
-import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.HoeItem;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.Hand;
 import nl.gjorgdy.solute.Solute;
 import nl.gjorgdy.solute.modules.Farmland;
 import org.spongepowered.asm.mixin.Mixin;
@@ -24,13 +28,27 @@ public class HoeItemMixin {
     public void onUse(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
         // early return if module disabled
         if (!Solute.CONFIG.farmlandModule.enabled) return;
-        // early return if module disabled
-        if (context.getWorld().getBlockState(context.getBlockPos()).getBlock() instanceof CropBlock && Farmland.farmArea(context)) {
-            PlayerEntity player = context.getPlayer();
+        // farm area if crops
+        var world = context.getWorld();
+        var pos = context.getBlockPos();
+        var player = context.getPlayer();
+        if (world.getBlockState(pos).getBlock() instanceof CropBlock && Farmland.farmArea(context)) {
             if (player != null) {
                 player.swingHand(context.getHand(), true);
             }
             cir.setReturnValue(ActionResult.SUCCESS);
+        }
+        // return farmland to dirt if empty
+        else if (world.getBlockState(pos).isOf(Blocks.FARMLAND)) {
+            if (world.getBlockState(pos.up()).isAir()) {
+                world.setBlockState(pos, Blocks.DIRT.getDefaultState());
+                world.playSound(null, pos, SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1f, 0.5f);
+                if (player != null) {
+                    context.getStack().damage(1, player, context.getHand() == Hand.MAIN_HAND ? EquipmentSlot.MAINHAND : EquipmentSlot.OFFHAND);
+                    player.swingHand(context.getHand(), true);
+                }
+                cir.setReturnValue(ActionResult.SUCCESS);
+            }
         }
     }
 
